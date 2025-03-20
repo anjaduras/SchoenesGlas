19b80f60921c39897b3b77e708004be9560f9900
anjaduras <anna@duras.org>
2025-03-20 10:05:28 +0100

    initial commit

---
+++ js/script.js
@@ -0,0 +1,6 @@
+function updateTime() {
+    const now = new Date();
+    const options = { day: '2-digit', month: '2-digit', year: 'numeric' };  // Displaying date as DD/MM/YYYY
+    document.getElementById("time").innerText = now.toLocaleDateString(undefined, options) + " " + now.toLocaleTimeString(undefined, { hour: '2-digit', minute: '2-digit' });
+}
+setInterval(updateTime, 1000);
