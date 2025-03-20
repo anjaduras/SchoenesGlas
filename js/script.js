750aade14a09c1490003b838a8ba2eb70a98b8e9
anjaduras <anna@duras.org>
2025-03-20 13:42:16 +0100

    intermidiate

--- js/script.js
+++ js/script.js
@@ -7,19 +7,4 @@ setInterval(updateTime, 1000);
 
 // no idea whatsoever
 function login() {
-    const email = document.getElementById("email").value;
-    const password = document.getElementById("password").value;
-    const errorMessage = document.getElementById("error-message");
-
-    fetch('users.json')
-        .then(response => response.json())
-        .then(data => {
-            const user = data.find(user => user.email === email && user.password === password);
-            if (user) {
-                window.location.href = "workers.html"; 
-            } else {
-                errorMessage.style.display = "block";
-            }
-        })
-        .catch(error => console.error("Error loading user data:", error));
 }
