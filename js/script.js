ff677208650027c9d1178062c125bdd47ff1a774
anjaduras <anna@duras.org>
2025-03-22 14:56:26 +0100

    ?

--- js/script.js
+++ js/script.js
@@ -1,25 +1,23 @@
 function updateTime() {
     const now = new Date();
     const options = { day: '2-digit', month: '2-digit', year: 'numeric' };  // Displaying date as DD/MM/YYYY
-    document.getElementById("time").innerText = now.toLocaleDateString(undefined, options) + " " + now.toLocaleTimeString(undefined, { hour: '2-digit', minute: '2-digit' });
+    document.getElementById("time").innerText = now.toLocaleDateString(undefined, options) + " " + now.toLocaleTimeString(undefined, { hour: '2-digit', minute: '2-digit', second: '2-digit'});
 }
 setInterval(updateTime, 1000);
-
-    // Contact
 
  // Login
   
   function handleAuth(event) {
     event.preventDefault();
   
-    const username = document.getElementById('username').value;
+    const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
     const errorMessage = document.getElementById('error-message');
   
-    const validUsername = 'user';
-    const validPassword = 'password';
+    const validEmail = 'user@SchoenesGlas.de';
+    const validPassword = '0ptikerKette';
   
-    if (username === validUsername && password === validPassword) {
+    if (email === validEmail && password === validPassword) {
       alert('Erfolgreich angemeldet!');
       window.location.href = './workers.html';
     } else {
@@ -27,3 +25,19 @@ setInterval(updateTime, 1000);
     }
   }
   
+  // Design
+
+  document.addEventListener("DOMContentLoaded", function () {
+    let buttons = document.querySelectorAll(".grid .startbutton");
+  
+    buttons.forEach(button => {
+      button.addEventListener("mouseover", function () {
+        this.style.transform = "translateY(-5px)";
+        this.style.transition = "transform 0.2s ease-in-out";
+      });
+  
+      button.addEventListener("mouseout", function () {
+        this.style.transform = "translateY(0)";
+      });
+    });
+  });
