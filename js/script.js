9f2d84c5d2d887df3e507fcd544fde08f9522dff
anjaduras <anna@duras.org>
2025-04-16 21:06:41 +0200

    hh

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 function updateTime() {
     const now = new Date();
-    const options = { day: '2-digit', month: '2-digit', year: 'numeric' };  // Displaying date as DD/MM/YYYY
+    const options = { day: '2-digit', month: '2-digit', year: 'numeric' };  
     document.getElementById("time").innerText = now.toLocaleDateString(undefined, options) + " " + now.toLocaleTimeString(undefined, { hour: '2-digit', minute: '2-digit', second: '2-digit'});
 }
 setInterval(updateTime, 1000);
